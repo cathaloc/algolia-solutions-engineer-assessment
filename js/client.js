b581333cc835720976c17256a607fc7da6b71b04
Cathal O Cuimin <cathalog@gmail.com>
2017-12-16 19:05:25 -0800

    Very basic setup. SCSS compilation setup using NPM.

---
+++ js/client.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Search from "./pages/Search"
+
+
+const app = document.getElementById('app');
+ReactDOM.render(<Search />, app);
---
+++ js/components/SearchFilters.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class SearchFilters extends React.Component {
+  render() {
+    return (
+      <div>
+        I am a SearchFilters
+      </div>
+    );
+  }
+}
